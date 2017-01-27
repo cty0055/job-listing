ec17bc3ad3867dd9ea531e520eaad679660b16c8
cty <email@xxx.xxx>
2017-01-27 14:21:16 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,6 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
-end
 
 def publish!
     self.is_hidden = false
@@ -14,4 +13,6 @@ def publish!
     self.is_hidden = true
     self.save
   end
-end  
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
+end
